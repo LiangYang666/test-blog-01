4abd38e80e5ffb343723638972c61fe3b696c64a
LiangYang666 <mail.liangyang@gmail.com>
2021-11-19 20:55:40 +0800

    1.1 建立完成工程

---
+++ src/main/java/com/liang/TestBlog01Application.java
@@ -0,0 +1,13 @@
+package com.liang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBlog01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBlog01Application.class, args);
+    }
+
+}
