9e3d57d53f2dcd81bb6c4a9e53463bd182e9e9f6
LiangYang666 <mail.liangyang@gmail.com>
2021-12-29 10:07:38 +0800

    2.2.4 在主启动类中添加包扫描

--- src/main/java/com/liang/TestBlog01Application.java
+++ src/main/java/com/liang/TestBlog01Application.java
@@ -1,13 +1,14 @@
 package com.liang;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.liang.modules.sys.dao")
 public class TestBlog01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(TestBlog01Application.class, args);
     }
-
 }
