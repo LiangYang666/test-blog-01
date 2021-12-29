6e3df0b5df0ee521d266e0d6733a131aa3356838
LiangYang666 <mail.liangyang@gmail.com>
2021-12-29 10:08:16 +0800

    2.2.4 测试类编写，进行测试UserDao

--- src/test/java/com/liang/TestBlog01ApplicationTests.java
+++ src/test/java/com/liang/TestBlog01ApplicationTests.java
@@ -1,13 +1,24 @@
 package com.liang;
 
+import com.liang.modules.sys.dao.UserDao;
+import com.liang.modules.sys.entity.UserEntity;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class TestBlog01ApplicationTests {
 
+    @Autowired
+    private UserDao userDao;
+
     @Test
     void contextLoads() {
+        List<UserEntity> usersEntities = userDao.selectList(null);
+        System.out.println(usersEntities);
     }
+
 
 }
