f14d61810fbde57d9262e29c35f1c9fc7cdcdade
LiangYang666 <mail.liangyang@gmail.com>
2022-01-02 09:28:55 +0800

    2.2.6 测试UserSerVice

--- src/test/java/com/liang/TestBlog01ApplicationTests.java
+++ src/test/java/com/liang/TestBlog01ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.liang;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.liang.modules.sys.dao.UserDao;
 import com.liang.modules.sys.dao.UserLoginDao;
 import com.liang.modules.sys.entity.UserEntity;
 import com.liang.modules.sys.entity.VO.UserVOEntity;
+import com.liang.modules.sys.service.UserService;
+import com.liang.modules.sys.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @SpringBootTest
@@ -27,6 +31,17 @@ class TestBlog01ApplicationTests {
     @Test
     void test2(){
         UserVOEntity byPhone = userLoginDao.findByPhone("12345678910");
+        System.out.println(byPhone.getUsername());
+        System.out.println(byPhone.getPassword());
+        System.out.println("====>Roles");
+        byPhone.getRoles().forEach(x-> System.out.println(x.getId()+" "+x.getRoleName()+ x.getPermissionSet()));
+    }
+
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+    @Test
+    void test3(){
+        UserVOEntity byPhone = userServiceImpl.findByPhone("12345678910");
         System.out.println(byPhone.getUsername());
         System.out.println(byPhone.getPassword());
         System.out.println("====>Roles");
