e888edd7ee708dcf7dd7cf7e405a0ebbb62937f5
LiangYang666 <mail.liangyang@gmail.com>
2022-01-02 09:39:05 +0800

    2.3.7 更改login.js的验证

--- src/main/resources/static/style/js/login.js
+++ src/main/resources/static/style/js/login.js
@@ -9,7 +9,7 @@ qzLogin.click(function () {
 function log(){
     var phone = $("#username").val().trim();
     var pass = $("#password").val();
-    var myreg = /^(((13[0-9]{1})|(14[0-9]{1})|(17[0]{1})|(15[0-3]{1})|(15[5-9]{1})|(18[0-9]{1}))+\d{8})$/;
+    var myreg = /^(((12[0-9]{1})|(13[0-9]{1})|(14[0-9]{1})|(17[0]{1})|(15[0-3]{1})|(15[5-9]{1})|(18[0-9]{1}))+\d{8})$/;
     if(phone.length == 0 || phone.length != 11 || !myreg.test(phone) ){
         $(".notice-box-res").show();
     } else if(pass.length < 6 || pass.length > 18){
