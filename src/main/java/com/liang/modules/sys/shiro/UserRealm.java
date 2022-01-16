dfa9983e3e81828ee3171c8787c2f4c3f24b6cba
LiangYang666 <mail.liangyang@gmail.com>
2022-01-16 19:36:23 +0800

    4.1.1 重写授权方法

--- src/main/java/com/liang/modules/sys/shiro/UserRealm.java
+++ src/main/java/com/liang/modules/sys/shiro/UserRealm.java
@@ -1,13 +1,21 @@
 package com.liang.modules.sys.shiro;
 
+import com.liang.modules.sys.entity.PermissionEntity;
+import com.liang.modules.sys.entity.VO.RoleVOEntity;
 import com.liang.modules.sys.entity.VO.UserVOEntity;
 import com.liang.modules.sys.service.UserService;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -24,12 +32,43 @@ public class UserRealm extends AuthorizingRealm {
 
     @Autowired
     private UserService userService;
-
+    
+     /**
+      * @method  授权方法
+      * @description 
+      * @date: 2022/1/16 18:47
+      * @author: Yang Liang
+      * @param null
+      * @return 
+      */
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        return null;
+        //获取用户(前期认证已经加入进去了)
+        UserVOEntity user = (UserVOEntity) SecurityUtils.getSubject().getPrincipal();
+        Set<RoleVOEntity> roles = user.getRoles();
+        List<String> roleNameList = new ArrayList<>();
+        List<String> permissionList = new ArrayList<>();
+        for (RoleVOEntity role :
+                roles) {
+            roleNameList.add(role.getRoleName());   // 添加角色名称
+            for (PermissionEntity permission:
+                 role.getPermissionSet()) {
+                permissionList.add(permission.getPermissionName()); // 添加权限名称
+            }
+        }
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();     // 授权
+        info.addStringPermissions(permissionList);
+        info.addRoles(roleNameList);
+        return info;
     }
-
+     /**
+      * @method  认证方法
+      * @description 
+      * @date: 2022/1/16 18:46
+      * @author: Yang Liang
+      * @param null
+      * @return 
+      */
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
         UsernamePasswordToken userToken = (UsernamePasswordToken) authenticationToken;
